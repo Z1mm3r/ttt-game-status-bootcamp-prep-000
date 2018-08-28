73bea96902b5835a29e5626e051dce073defa66f
Casey Zimmer <czimmer@ramapo.edu>
2018-08-28 00:44:31 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,7 @@ WIN_COMBINATIONS = [
 def won(board)
   
   WIN_COMBINATIONS.select do |element|
-    if (board[element[0] == "X" &&)
+    if (board[element[0]] == "X" && board[element[1]] == "X" && board[element[2]] == "X")
   end
   
 end
